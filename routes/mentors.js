968ff6c2d2c8f51daec2493ce31b7c94e28f7248
Septian Adhitya <dsrisk911@gmail.com>
2022-07-27 17:05:58 +0700

    add api update Mentors

--- routes/mentors.js
+++ routes/mentors.js
@@ -6,5 +6,6 @@ const mentorsHandler = require("./handler/mentors");
 router.get("/", mentorsHandler.getAll);
 router.get("/:id", mentorsHandler.get);
 router.post("/", mentorsHandler.create);
+router.put("/:id", mentorsHandler.update);
 
 module.exports = router;
