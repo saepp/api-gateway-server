4abf2b42b8dc33a8c7a5f0bb4c7a2e7c7d078845
Septian Adhitya <dsrisk911@gmail.com>
2022-08-13 14:38:37 +0700

    add: api delete Reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,5 +5,6 @@ const reviewsHandler = require("./handler/reviews");
 
 router.post("/", reviewsHandler.create);
 router.put("/:id", reviewsHandler.update);
+router.delete("/:id", reviewsHandler.destroy);
 
 module.exports = router;
