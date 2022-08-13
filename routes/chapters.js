2f95c46211a1da7b45c3171a2b9441070bc3741b
Septian Adhitya <dsrisk911@gmail.com>
2022-08-13 13:29:38 +0700

    add: api get list Chapters

--- routes/chapters.js
+++ routes/chapters.js
@@ -7,5 +7,6 @@ router.post("/", chaptersHandler.create);
 router.put("/:id", chaptersHandler.update);
 router.delete("/:id", chaptersHandler.destroy);
 router.get("/:id", chaptersHandler.get);
+router.get("/", chaptersHandler.getAll);
 
 module.exports = router;
