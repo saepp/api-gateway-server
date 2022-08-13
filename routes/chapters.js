bb90bbfb6195bbfa1ae963dffd2db5e6af1f3063
Septian Adhitya <dsrisk911@gmail.com>
2022-08-13 13:22:22 +0700

    add: api update Chapters

--- routes/chapters.js
+++ routes/chapters.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const chaptersHandler = require("./handler/chapters");
 
 router.post("/", chaptersHandler.create);
+router.put("/:id", chaptersHandler.update);
 
 module.exports = router;
