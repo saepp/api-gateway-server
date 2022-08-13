1eefb38f2c6fd7b611bc5c1418cc7f883b80938a
Septian Adhitya <dsrisk911@gmail.com>
2022-08-13 14:02:31 +0700

    add: api delete imageCourses

--- routes/imageCourses.js
+++ routes/imageCourses.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const imageCoursesHandler = require("./handler/image-courses");
 
 router.post("/", imageCoursesHandler.create);
+router.delete("/:id", imageCoursesHandler.destroy);
 
 module.exports = router;
